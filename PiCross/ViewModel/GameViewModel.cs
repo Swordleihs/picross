0c8b479f7b9a8edad16f63c4ca398637c11880de
Ruben Claes <ruben.claes@student.ucll.be>
2019-05-20 21:12:00 +0200

    implemented viewModel classes

---
+++ PiCross/ViewModel/GameViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Cells;
+using DataStructures;
+using PiCross;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class GameViewModel : INotifyPropertyChanged
+    {
+        private readonly IPlayablePuzzle playablePuzzle;
+
+        public GameViewModel(Puzzle puzzle)
+        {
+            var facade = new PiCrossFacade();
+            this.playablePuzzle = facade.CreatePlayablePuzzle(puzzle);
+
+            this.Grid = this.playablePuzzle.Grid.Map(square => new SquareViewModel(square)).Copy();
+            this.RowConstraints = this.playablePuzzle.RowConstraints.Map(row => new RowViewModel(row)).Copy();
+            this.ColumnConstraints = this.playablePuzzle.ColumnConstraints.Map(column => new ColumnViewModel(column)).Copy();
+            this.IsSatisfied = this.playablePuzzle.IsSolved;
+        }
+
+        public object Grid { get; }
+
+        public object RowConstraints { get; }
+        public object ColumnConstraints { get; }
+
+        public Cell<bool> IsSatisfied { get; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class RowViewModel
+    {
+        private IPlayablePuzzleConstraints row;
+
+        public RowViewModel(IPlayablePuzzleConstraints row)
+        {
+            this.row = row;
+            this.IsSatisfied = row.IsSatisfied;
+        }
+
+        public object Values => row.Values;
+
+        Cell<bool> IsSatisfied { get; }
+    }
+
+    public class ColumnViewModel
+    {
+        private IPlayablePuzzleConstraints column;
+
+        public ColumnViewModel(IPlayablePuzzleConstraints column)
+        {
+            this.column = column;
+            this.IsSatisfied = column.IsSatisfied;
+        }
+
+        public object Values => column.Values;
+
+        Cell<bool> IsSatisfied { get; }
+    }
+}
---
+++ PiCross/ViewModel/SquareViewModel.cs
@@ -0,0 +1,90 @@
+﻿using PiCross;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class SquareViewModel : INotifyPropertyChanged
+    {
+        private readonly IPlayablePuzzleSquare square;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand SquareLeftClick { get; }
+        public ICommand SquareRightClick { get; }
+
+        public SquareViewModel(IPlayablePuzzleSquare square)
+        {
+            this.square = square;
+            this.SquareLeftClick = new SquareLeftClickCommand(this);
+            this.SquareRightClick = new SquareRightClickCommand(this);
+        }
+
+        public object Contents
+        {
+            get
+            {
+                return square.Contents;
+            }
+        }
+
+        private class SquareLeftClickCommand : ICommand
+        {
+            private SquareViewModel _viewModel;
+
+            public SquareLeftClickCommand(SquareViewModel vm)
+            {
+                _viewModel = vm;
+
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public void Execute(object parameter)
+            {
+                if (_viewModel.square.Contents.Value == Square.FILLED)
+                {
+                    _viewModel.square.Contents.Value = Square.EMPTY;
+                }
+                else if(_viewModel.square.Contents.Value == Square.EMPTY) {
+                    _viewModel.square.Contents.Value = Square.FILLED;
+                }
+                else
+                {
+                    _viewModel.square.Contents.Value = Square.FILLED;
+                }
+
+            }
+        }
+        private class SquareRightClickCommand : ICommand
+        {
+            private SquareViewModel _viewModel;
+
+            public SquareRightClickCommand(SquareViewModel vm)
+            {
+                _viewModel = vm;
+
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public void Execute(object parameter)
+            {
+                _viewModel.square.Contents.Value = Square.UNKNOWN;
+            }
+        }
+    }   
+}
